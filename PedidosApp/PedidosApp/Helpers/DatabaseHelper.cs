74a13efae468145d6e250246e78f948ac2376894
aslac2020 <andredesenvolvedorti@gmail.com>
2021-03-08 23:57:30 -0300

    primeiro commit

---
+++ PedidosApp/PedidosApp/Helpers/DatabaseHelper.cs
@@ -0,0 +1,65 @@
+﻿using PCLExt.FileStorage;
+using PCLExt.FileStorage.Folders;
+using PedidosApp.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PedidosApp.Helpers
+{
+    public class DatabaseHelper
+    {
+        //conexão com banco de dados
+        static SQLiteConnection sqliteconnection;
+        public const string DbFileName = "PedidosDB.db";
+
+        public DatabaseHelper()
+        {
+            //cria pasta base local para o dispositivo
+            var pasta = new LocalRootFolder();
+            //cria o arquivo
+            var arquivo = pasta.CreateFile(DbFileName, CreationCollisionOption.OpenIfExists);
+            //abre o BD
+            sqliteconnection = new SQLiteConnection(arquivo.Path);
+            //cria a tabela no BD
+            sqliteconnection.CreateTable<Pedido>();
+        }
+
+        //Pegar todos os dados
+        public List<Pedido> GetAllPedidosData()
+        {
+            return (from data in sqliteconnection.Table<Pedido>() select data).ToList();
+        }
+
+        //Pegar dados pelo Id
+        public Pedido GetPedidoData(int id)
+        {
+            return sqliteconnection.Table<Pedido>().FirstOrDefault(p => p.Id == id);
+        }
+
+        //Deletar todos os dados
+        public void DeleteAllPedidos()
+        {
+            sqliteconnection.DeleteAll<Pedido>();
+        }
+
+        //Deletar dados pelo Id
+        public void DeletePedido(int id)
+        {
+            sqliteconnection.Delete<Pedido>(id);
+        }
+
+        //Inserir dados
+        public void InsertPedido(Pedido pedido)
+        {
+            sqliteconnection.Insert(pedido);
+        }
+
+        //Atualizar dados
+        public void IUpdatePedido(Pedido pedido)
+        {
+            sqliteconnection.Update(pedido);
+        }
+
+    }
+}
---
+++ PedidosApp/PedidosApp/Validator/PedidoValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using PedidosApp.Models;
+using System;
+
+namespace PedidosApp.Validator
+{
+    public class PedidoValidator : AbstractValidator<Pedido>
+    {
+        public PedidoValidator()
+        {
+            RuleFor(c => c.Titulo).Must(n => ValidateStringEmpty(n)).WithMessage("o nome não pode ser vazio");
+            RuleFor(c => c.Link).Must(n => ValidateStringEmpty(n)).WithMessage("o link não pode ser vazio");
+            RuleFor(c => c.Preco).NotEmpty().WithMessage("Informe o preço").Must(PrecoMaiorQueZero).WithMessage("o valor deve ser maior que zero");
+            RuleFor(c => c.Descricao).Must(n => ValidateStringEmpty(n)).WithMessage("o nome não pode ser vazio");
+            RuleFor(c => c.Titulo).Must(n => ValidateStringEmpty(n)).WithMessage("a desrição não pode estar em branco");
+        }
+
+        private static bool PrecoMaiorQueZero(decimal preco)
+        {
+            return preco > 0;
+        }
+
+        bool ValidateStringEmpty(string stringValue)
+        {
+            if (!string.IsNullOrEmpty(stringValue))
+                return true;
+            return false;
+        }
+    }
+}
