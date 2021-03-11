8f5ff943fa1979ec443df68fe906f69cee57038e
aslac2020 <andredesenvolvedorti@gmail.com>
2021-03-10 22:42:58 -0300

    alterações

--- PedidosApp/PedidosApp/Services/INavigationService.cs
+++ PedidosApp/PedidosApp/Services/INavigationService.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PedidosApp.Services
 {
    public interface INavigationService
     {
+        Task NavigateToAddPedido();
+        Task NavigateToDetailsPage(int ID);
+        Task NavigateToPedidoLista();
+        void PopAsyncService();
+
     }
 }
---
+++ PedidosApp/PedidosApp/Services/NavigationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace PedidosApp.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public async Task NavigateToAddPedido()
+        {
+            await PedidosApp.App.Current.MainPage.Navigation.PushAsync(new AddPedido());
+        }
+        public async Task NavigateToDetailsPage(int ID)
+        {
+            await PedidosApp.App.Current.MainPage.Navigation.PushAsync(new DetailsPage(ID));
+        }
+        public async Task NavigateToPedidoLista(int ID)
+        {
+            await PedidosApp.App.Current.MainPage.Navigation.PushAsync(new PedidoLista(ID));
+        }
+        public async void PopAsyncService()
+        {
+            await PedidosApp.App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
